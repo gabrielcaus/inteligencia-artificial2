78d39bd6a6d079e1639d2009183f31b6edc751cf
gabrielcaus <108276059+gabrielcaus@users.noreply.github.com>
2024-08-22 10:18:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -103,18 +103,3 @@ function mostraAlternativas() {
   }
   
 mostraPergunta();
-
-function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    mostraAlternativas();
-}
-function mostraAlternativas() {
-    for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativa.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
-}
-
-mostraPergunta();
